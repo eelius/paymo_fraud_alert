7eead43deb6d91d04f38abad0601f0ff2dce9c91
eelius <eetienn02@gmail.com>
2016-11-10 18:24:32 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
+# code to compile then execute the program using java
+# input files: paymo_input/batch_payment.csv paymo_input/stream_payment.csv
+# output files: paymo_output/output1.txt paymo_output/output2.txt paymo_output/output3.txt
+
 javac src/*.java
 java -classpath src paymoFraudCheck paymo_input/batch_payment.csv paymo_input/stream_payment.csv paymo_output/output1.txt paymo_output/output2.txt paymo_output/output3.txt
